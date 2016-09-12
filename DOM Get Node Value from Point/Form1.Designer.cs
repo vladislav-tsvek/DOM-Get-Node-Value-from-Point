cc1bd1dd83b230be7493a5731584a5d1a9a0bdc6
vladislav.tsvek <vladislav.tsvek@teamdev.com>
2016-09-12 12:21:27 +0300

    Returns node name by pressing right mouse button.

--- DOM Get Node Value from Point/Form1.Designer.cs
+++ DOM Get Node Value from Point/Form1.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripGetElement = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabelX = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabelY = new System.Windows.Forms.ToolStripLabel();
+            this.browserView = new DotNetBrowser.WinForms.WinFormsBrowserView();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripGetElement,
+            this.toolStripLabelX,
+            this.toolStripLabelY});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(922, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripGetElement
+            // 
+            this.toolStripGetElement.Name = "toolStripGetElement";
+            this.toolStripGetElement.Size = new System.Drawing.Size(113, 22);
+            this.toolStripGetElement.Text = "toolStripGetElement";
+            // 
+            // toolStripLabelX
+            // 
+            this.toolStripLabelX.Name = "toolStripLabelX";
+            this.toolStripLabelX.Size = new System.Drawing.Size(87, 22);
+            this.toolStripLabelX.Text = "toolStripLabelX";
+            // 
+            // toolStripLabelY
+            // 
+            this.toolStripLabelY.Name = "toolStripLabelY";
+            this.toolStripLabelY.Size = new System.Drawing.Size(87, 22);
+            this.toolStripLabelY.Text = "toolStripLabelY";
+            // 
+            // browserView
+            // 
+            this.browserView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browserView.Location = new System.Drawing.Point(0, 25);
+            this.browserView.Name = "browserView";
+            this.browserView.Size = new System.Drawing.Size(922, 478);
+            this.browserView.TabIndex = 1;
+            this.browserView.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 503);
+            this.Controls.Add(this.browserView);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripGetElement;
+        private DotNetBrowser.WinForms.WinFormsBrowserView browserView;
+        private System.Windows.Forms.ToolStripLabel toolStripLabelX;
+        private System.Windows.Forms.ToolStripLabel toolStripLabelY;
     }
 }
 
--- DOM Get Node Value from Point/Form1.cs
+++ DOM Get Node Value from Point/Form1.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using DotNetBrowser.DOM;
+using DotNetBrowser.Events;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace DOM_Get_Node_Value_from_Point
 {
@@ -15,6 +19,52 @@ namespace DOM_Get_Node_Value_from_Point
         public Form1()
         {
             InitializeComponent();
+           
+        }
+
+        private void Form1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            //Check for right_click mouse button
+            if (e.Button == MouseButtons.Right)
+            {
+                //For debug
+                toolStripLabelX.Text = "X: " + e.X.ToString();
+                toolStripLabelY.Text = "Y: " + e.Location.Y.ToString();
+
+                Point dot = new Point(e.X, e.Y);
+                
+                DOMNodeAtPoint point = browserView.Browser.NodeAtPoint(dot);
+
+                try
+                {
+                    toolStripGetElement.Text = "Node name: " + point.Node.NodeName.ToString();
+
+                }
+                catch { }
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ManualResetEvent resetEvent = new ManualResetEvent(false);
+            FinishLoadingFrameHandler listener = new FinishLoadingFrameHandler((object sender1, FinishLoadingEventArgs e1) =>
+            {
+                if (e1.IsMainFrame)
+                {
+                    resetEvent.Set();
+                }
+            });
+            browserView.Browser.FinishLoadingFrameEvent += listener;
+            try
+            {
+                browserView.Browser.LoadURL("teamdev.com");
+                resetEvent.WaitOne(new TimeSpan(0, 0, 45));
+            }
+            finally
+            {
+                browserView.Browser.FinishLoadingFrameEvent -= listener;
+            }
         }
     }
 }
